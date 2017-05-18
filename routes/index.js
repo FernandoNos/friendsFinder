09c66cfbd4c6732f2021297680cdb0b23595ef0c
Fernando <fernandonos@gmail.com>
2017-05-18 18:51:50 -0300

    Getting user's likes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var user = require("../biz/user");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	user.get_user_likes().then(function(response){
+		var next = response.json.paging.cursors.after;
+		var previous = response.json.paging.cursors.before;
+		var data = response.json.data;
+		console.log(next);
+		console.log(previous);
+		res.send(response.json);
+	});
+});
+
+router.get('/index',function(req,res,next){
+	res.sendFile('../views/index.dust');
 });
 
 module.exports = router;
